bc23dbead98f76931aec3185c0f045a42ef395c3
weite122 <949193531@qq.com>
2019-07-17 03:56:38 +0800

    [A]动态路由，嵌套路由，命名路由

--- vue-course/vue-router/src/router/router.js
+++ vue-course/vue-router/src/router/router.js
@@ -14,5 +14,21 @@ export default [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+  },
+  // 动态路由
+  {
+    path: '/argu/:name',
+    component: () => import('@/views/argu.vue')
+  },
+  // 嵌套路由
+  {
+    path: '/parent',
+    component: () => import('@/views/parent.vue'),
+    children: [
+      {
+        path: 'child',
+        component: () => import('@/views/child.vue')
+      }
+    ]
   }
 ]

0008f49bc2cfab0c8b4d8eb218f27077cf607607
weite122 <949193531@qq.com>
2019-07-17 10:06:47 +0800

    [A]重定向

--- vue-course/vue-router/src/router/router.js
+++ vue-course/vue-router/src/router/router.js
@@ -30,5 +30,19 @@ export default [
         component: () => import('@/views/child.vue')
       }
     ]
+  },
+  {
+    path: '/named_view',
+    components: {
+      default: () => import('@/views/child.vue'),
+      email: () => import('@/views/email.vue'),
+      tel: () => import('@/views/tel.vue')
+    }
+  },
+  {
+    path: '/main',
+    redirect: to => {
+      return 'home'
+    }
   }
 ]

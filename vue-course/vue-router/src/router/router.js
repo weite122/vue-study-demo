aabf46dfd82c1467b2bfeacafa77f22a873da721
weite122 <949193531@qq.com>
2019-07-19 04:09:45 +0800

    [A]父子组件通信

--- vue-course/vue-router/src/router/router.js
+++ vue-course/vue-router/src/router/router.js
@@ -68,6 +68,10 @@ export default [
     }
   },
   {
+    path: '/store',
+    component: () => import('@/views/store.vue')
+  },
+  {
     path: '*',
     component: () => import('@/views/error_404.vue')
   }
---
+++ vue-course/vue-router/src/views/store.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <a-input v-model="inputValue"/>
+      <h1>{{ inputValue }}</h1>
+  </div>  
+</template>
+
+<script>
+  import AInput from '_c/AInput.vue'
+  export default {
+    name: 'store',
+    data() {
+      return {
+        inputValue: ''
+      }
+    },
+    components: {
+      AInput
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

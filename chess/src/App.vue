b86588be566ce9a219772833d2caacf3eac8078b
weite122 <949193531@qq.com>
2019-07-31 22:05:14 +0800

    [A]chess项目

---
+++ chess/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="row">
+            <Cell @click="greet"/> 
+            <Cell /> 
+            <Cell />  
+        </div>
+        <div class="row">
+            <Cell /> 
+            <Cell /> 
+            <Cell /> 
+        </div>        
+        <div class="row">
+            <Cell /> 
+            <Cell /> 
+            <Cell /> 
+        </div>
+    </div>
+</template>
+
+<script>
+import Cell from './Cell.vue'
+
+export default {
+  components: {
+    Cell
+  },
+  methods: {
+      greet() {
+          console.log('xxx')
+      }
+  }
+}
+</script>
+
+<style>
+.row {
+    display: flex;
+}
+</style>
---
+++ chess/src/Cell.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="cell" @click="y">
+      <template v-if="a">{{ text }}</template>
+      <template v-else></template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { a: false, text: "o" };
+  },
+  methods: {
+      y() {
+          this.a = true
+          this.$emit('click')
+      }
+  }
+};
+</script>
+
+<style>
+.cell {
+  border: 1px solid black;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 80px;
+}
+</style>

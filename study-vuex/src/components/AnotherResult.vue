a445b89b90ec80f1088dc0c68a982e710d826c91
Tu Wenbin <949193531@qq.com>
2020-12-02 23:43:17 +0800

    Create AnotherResult.vue

---
+++ study-vuex/src/components/AnotherResult.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>当前计数: {{ doubleCounter }}</p>
+    <p>当前次数: {{ stringCounter }}</p>
+  </div>
+</template>
+
+<script>
+
+import {mapGetters} from 'vuex'
+
+export default {
+  // name: "Result",
+  // // props: ['counter']
+  // computed: {
+  //   counter() {
+  //     return this.$store.getters.doubleCounter
+  //   },
+  //   clicks() {
+  //     return this.$store.getters.stringCounter
+  //   }
+  // }
+  computed: {
+    ...mapGetters(['doubleCounter', 'stringCounter']),
+    myOwnComputedProp(){}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
